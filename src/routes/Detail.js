b95cd121e58c2beb5ae30fc6037303bc0272f52a
seungin yang <rhfpdk92@naver.com>
2020-10-30 11:22:49 +0900

    movie details

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,7 @@
 import { func } from "prop-types";
 import React from "react";
 import { Redirect } from "react-router-dom";
+import "./Detail.css";
 
 class Detail extends React.Component {
   componentDidMount() {
@@ -12,12 +13,35 @@ class Detail extends React.Component {
   }
   render() {
     const { location } = this.props;
+    console.log(location.state); //year title, summary, poster, genres
     if (location.state) {
-      return <span>{location.state.title}</span>;
+      return (
+        <section className='movieDetail'>
+          <img
+            src={location.state.poster}
+            alt={location.state.title}
+            title={location.state.title}
+          ></img>
+          <div className='movieContent'>
+            <h3 className='movieContent__title'>{location.state.title}</h3>
+            <h5 className='movieContent__year'>{location.state.year}</h5>
+            <ul className='movieContent_genres'>
+              {location.state.genres.map((genre, index) => (
+                <li key={index} className='movieContent__genre'>
+                  {genre}
+                </li>
+              ))}
+            </ul>
+            <p className='movieContent__summary'>{location.state.summary}</p>
+          </div>
+        </section>
+      );
     } else {
       return null;
     }
   }
 }
+
+// Detail.prototype ={}
 
 export default Detail;
